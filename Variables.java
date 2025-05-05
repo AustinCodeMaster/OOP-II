25e206fa42ce7d179b8179b6f7e3515642ea2dde
AustinCodeMaster <austinndiangui.com>
2025-05-05 14:34:13 +0300

    A program that uses variables to calculate the interest over a period of 2 years

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    void showIntrest(){
+        double amount;
+        double interest;
+
+        amount=1200.00;
+        System.out.println("Amount: " + amount);
+        
+        interest=3.00;
+        amount=amount * (100+interest)/100;
+        System.out.println("after a year: " + amount);
+
+        amount=amount *(100+interest)/100;
+        System.out.println("after 2 years: " + amount);
+    }
+        public static void main(String[] args){
+            (new Variables()).showIntrest();
+        }
+    }
+
